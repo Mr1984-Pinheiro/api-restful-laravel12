4aee9ccb12e89e300f73c429c8ae46b12118c067
Carlos Eduardo Tavares Pinheiro <cadutapinheiro@gmail.com>
2026-01-13 15:22:10 -0300

    Refactor code structure for improved readability and maintainability

---
+++ app/Filament/Resources/Customers/Schemas/CustomerForm.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\Customers\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class CustomerForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required()
+                    ->label('Name'),
+                TextInput::make('phone')
+                    ->required()
+                    ->label('Phone'),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Customers/Tables/CustomersTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Resources\Customers\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class CustomersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('phone')
+                    ->sortable()
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
